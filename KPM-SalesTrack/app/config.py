e343a6c7d72e773cc197af895917148d20de0bc5
CodexLante <allomwas@gmail.com>
2025-10-20 14:35:08 +0300

    initializing Flask

---
+++ KPM-SalesTrack/app/config.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv 
+
+# loading enviroment variables
+
+load_dotenv()
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SQLALCHEMY_TRACK_MODIFICATIONs = True
