ba0910d2046a1a07acf6f7c29ed3a89654271c77
Eric Mbithi <mbithieri@gmail.com>
2025-10-20 17:34:34 +0300

    Implementing user login and token generation with JWT

--- KPM-SalesTrack/app/routes/user.py
+++ KPM-SalesTrack/app/routes/user.py
@@ -39,3 +39,66 @@ def add_users():
   exists=users.query.filter_by(email=email).first()
   if exists:
     return jsonify({"error":"User with this email already exists"}),400
+
+  hashed_password = bycrypt.generate_password_hash(password).decode('utf-8')
+
+  new_users = users(username=username,email=email,password=hashed_password,is_active=is_active,created_at=created_at)
+  db.session.add(new_user)
+  db.session.commit()
+
+  access_token = create_access_token(
+    identity=str(new_users.id),
+    additional_claims={"email": new_users.email},
+    expires_delta=timedelta(hours=1)
+  )
+
+  return jsonify({
+    "message": "User created successfully",
+    "token": access_token,
+    "user": {
+      "id" : new_users.id,
+      "first_name": new_users.first_name,
+      "last_name": new_users.last_name,
+      "email": new_users.email,
+      "is_active": new_users.is_active,
+      "created_at": new_users.created_at
+    }
+  })
+
+  @users_bp.route("/login", methods=["POST"])
+  def login_users():
+    data = request.get_json()
+
+    email = data.get("email")
+    password = data.get("password")
+
+    if not email or not password:
+      return jsonify({"error": "Email and password are required"}), 400
+
+    users = users.query.filter_by(email=email).first()
+
+    if not users:
+      return jsonify({"error": "user not found"}), 401
+
+    check_password = bcrypt.check_password_hash(users.password, password)
+
+    if not check_password:
+      return jsonify({"error": "Invalid email or password"}), 401
+
+    access_token = create_access_token(
+      identity=str(users.id),
+      additional_claims={"email": new_users.email},
+      expires_delta=timedelta(hours=1)
+    )
+
+    return jsonify({
+      "token": access_token,
+      "user": {
+        "id": users.id,
+        "first_name": users.first_name,
+        "last_name": users.last_name,
+        "email": users.email,
+        "is_active": users.is_active,
+        "created_at": users.created_at
+      }
+    }), 200
