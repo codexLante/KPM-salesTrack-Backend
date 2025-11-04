d4e4cc619e39bc89d6ab08a99767e40ec5aef699
CodexLante <allomwas@gmail.com>
2025-11-04 14:51:25 +0300

    Adding essentials for hosting

--- KPM-SalesTrack/main.py
+++ KPM-SalesTrack/main.py
@@ -11,5 +11,10 @@ app=create_app()
 CORS(app)
 
 
+@app.route("/")
+def home():
+    return "Welcome KPM salesTrack"
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
